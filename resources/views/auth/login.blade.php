2fb9e4390cc97626d66ed3c543040360c6290d17
jacksoncarelli <jacksoncarelli@gmail.com>
2015-11-26 18:55:02 +1400

    novo form login
    novo form login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,36 +1,19 @@
 @extends('layouts.templatelogin	')
 
 @section('content')
-	<head xmlns="http://www.w3.org/1999/html">
 
-		<style type="text/css">
-			body {
-				background-image: url("/assets/img/login.jpg");
-				background-repeat: no-repeat;
-				background-attachment: fixed;
-				background-size: cover;
-			}
 
-		</style>
-</head>
-	<body
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
+	<!-- Form Module-->
+	<div class="module form-module" >
+		<div>
+		</div>
+		<div class="form left" >
+			<h2 align="center">Área de login</h2>
 
-<div class="container-fluid" >
-	<div class="row">
-		<div style="margin-top: 1%" top class="col-md-3 col-md-offset-1">
-			<div class="panel panel-default">
-				<h2><div align="center" class="panel-heading">Área de login<div></h2>
+			<form class="form-group" role="form" method="POST" action="{{ url('/auth/login') }}">
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
 									<li>{{ $error }}</li>
@@ -39,45 +22,27 @@
 						</div>
 					@endif
 
-					<form class="form-group" role="form" method="POST" action="{{ url('/auth/login') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+						<div class="input-group">
+							<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+							<input type="email" class="form-control" name="email" value="{{ old('Usuario') }}" autofocus>
+						</div><br>
 						<div class="form-group">
-							<label for="exampleInputEmail1">Email do usuário</label>
 							<div class="input-group">
-								<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-								<input type="email" class="form-control" name="email" value="{{ old('Usuario') }}" autofocus>
+								<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+								<input type="password" class="form-control" name="password">
 							</div>
-
-							<div class="form-group">
-								<label for="exampleInputPassword1">Senha</label>
-								<div class="input-group">
-									<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-									<input type="password" class="form-control" name="password">
-								</div>
-
-
-						<div class="form-group">
-							<div align="center">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Lembrar-me
-									</label>
-								</div>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div align="center">
-								<button type="submit" class="btn btn-primary">Entrar</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
+<br>
+				<button>Login</button>
+			</form>
 		</div>
+		<div class="form">
+
+
+		</div>
+		<div ></div>
 	</div>
-</div>
-	</body>
+	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+	<script src='loginform/js/da0415260bc83974687e3f9ae.js'></script>
 
 @endsection
